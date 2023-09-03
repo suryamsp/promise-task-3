c1bbb95377667ff4904fe84c74ff3f04360fe630
suryamsp <murugesansuryamsp@gmail.com>
2023-09-02 19:40:21 -0700

    promise_task3

--- script.js
+++ script.js
@@ -67,38 +67,8 @@ function Promise_Request(url) {
     document.querySelector('div').append(cer_div3)
 
     })
-    // .catch(error => {
-    //   console.log('Error:', error.message);
-    // });
-
-  Promise_Request('https://dog.ceo/api/breeds/image/random')
-    .then(dog => {
-
-      var text= dog.message
-      var stext= text.slice(30)
-      var result=[];
-      for (var i=0; i<stext.length; i++) {
-          if(stext[i] != '/'){
-            result.push(stext[i]);
-          }else{
-            break;
-          }
-
-      }  
-      var dog_name=result.join("");
-    var cer_div= document.createElement("div");
-    cer_div.innerHTML=`
-
-    <div class="small-con">
-<img src=${dog.message} />
-<h2>${dog_name}</h2>
-
-<div class="btn">   <button onClick="window.location.reload();">Next</button></div>
-</div>  `
-    document.querySelector('div').append(cer_div)
-
-    })
     .catch(error => {
       console.log('Error:', error.message);
     });
 
+ 
